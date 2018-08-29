27c0d840a31374fd315013379371b7520ea1d778
atomsk752 <bigman01kr@naver.com>
2018-08-30 00:25:17 +0900

    horse name changed

--- src/org/atomsk/Main.java
+++ src/org/atomsk/Main.java
@@ -8,12 +8,12 @@ public class Main {
         arr[0] = new Horse("양말");
         arr[1] = new Horse("그짓말");
         arr[2] = new Horse("아무말1");
-        arr[3] = new Horse("아무말2");
-        arr[4] = new Horse("아무말3");
-        arr[5] = new Horse("아무말4");
-        arr[6] = new Horse("아무말5");
-        arr[7] = new Horse("아무말6");
-        arr[8] = new Horse("아무말7");
+        arr[3] = new Horse("정말");
+        arr[4] = new Horse("사랑한단말");
+        arr[5] = new Horse("암흑의다크니스");
+        arr[6] = new Horse("전설의레전드");
+        arr[7] = new Horse("말말말");
+        arr[8] = new Horse("뭔말");
         arr[9] = new Horse("아무말8");
 
 
